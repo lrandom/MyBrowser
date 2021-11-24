f6b036f27e961b337be19fae1dbfa8f6c5923b4a
Nguyen Thanh Luan <luann4099@gmail.com>
2021-11-24 21:03:56 +0700

    update get data from implicit intent

--- app/src/main/java/com/example/mybrowser/MainActivity.java
+++ app/src/main/java/com/example/mybrowser/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mybrowser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Uri uri = getIntent().getData();
+        String uriString = uri.toString();
+        String extraData = getIntent().getStringExtra("extra_data");
+        Toast.makeText(this, extraData + uriString, Toast.LENGTH_SHORT).show();
     }
 }
